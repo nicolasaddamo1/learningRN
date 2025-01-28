edd5c20d1ef9bdb95eb8adf97a1ba4d6244aa873
Nico <nicolasaddamo1@gmail.com>
2025-01-28 15:12:13 -0300

    added Pressable

--- reactNative0/App.js
+++ reactNative0/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableHighlight, Pressable } from 'react-native';
 // import icon from './assets/icon.png'; es lo mismo que el require
 const icon = require('./assets/icon.png')
 export default function App() {
@@ -16,19 +16,61 @@ export default function App() {
       }}
       />
       <Image 
-        source={{uri:'https://www.metacritic.com/a/img/catalog/provider/6/3/6-1-4763-13.jpg'}}
-        style={{
-          width:215,
-          height:294,
-          resizeMode:'contain',
-          //las imagenes que son remotas no se muestran hasta que le pongas una resolucion de ancho y alto //!ESTO ES OBLIGATORIO!
-        }}
+          source={{uri:'https://www.metacritic.com/a/img/catalog/provider/6/3/6-1-4763-13.jpg'}}
+          style={{
+            width:215,
+            height:294,
+            resizeMode:'contain',
+            //las imagenes que son remotas no se muestran hasta que le pongas una resolucion de ancho y alto //!ESTO ES OBLIGATORIO!
+          }}
       />
       <Text
-      style={{color:'white'}}>Un cambio!</Text>
+          style={{color:'white'}}>
+          Un cambio!
+      </Text>
 
-      <Button title='Click me!' onPress={()=>alert('Hola!')} />//!ESTE BOTON ES EL NATIVO, NO SE PUEDE ESTILAR
-      <StatusBar style="light" />
+      <Button //!ESTE BOTON ES EL NATIVO, NO SE PUEDE ESTILAR
+          title='Click me!'
+          onPress={
+            ()=>alert('Hola!')
+          }
+      />
+      <TouchableHighlight //!ESTE BOTON SI SE PUEDE ESTILAR
+          underlayColor={'#fff'}
+          style={{
+            backgroundColor:'red',
+            padding:20,
+            borderRadius:10,
+          }}
+          onPress={
+            ()=>alert('Hola!')
+          }
+      >
+        <Text>Pulsa aqui!</Text>
+      </TouchableHighlight>
+
+      <Pressable
+        onPress={
+          ()=>{}
+        }
+        style={({pressed})=>[
+          {
+            backgroundColor:pressed?'red':'blue'
+          },
+          styles.wrappedCustom,
+          ]}>
+            {({pressed})=>(
+              <Text style={{
+                fontSize: pressed? 32:16,
+                color: pressed ? 'gray':'white'}}>
+                {pressed?'Presionado':'Presiona aqui!'}
+
+              </Text>
+            )}
+          </Pressable>
+      <StatusBar
+          style="light"
+      />
     </View>
   );
 }
