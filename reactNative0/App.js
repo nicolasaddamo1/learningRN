b9f32914d20905247b264ec1bf192d7b5567d704
Nico <nicolasaddamo1@gmail.com>
2025-01-28 16:53:46 -0300

    rendering pokes

--- reactNative0/App.js
+++ reactNative0/App.js
@@ -1,78 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Button,
-  TouchableHighlight,
-  Pressable,
-} from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { getLatestGames } from "./lib/metacritic";
 // import icon from './assets/icon.png'; es lo mismo que el require
-const icon = require("./assets/icon.png");
+// const icon = require("./assets/icon.png");
 export default function App() {
+  const [pokemon, setPokemon] = useState([]);
+  useEffect(() => {
+    getLatestGames().then((pokemon) => {
+      setPokemon(pokemon);
+    });
+  }, []);
   return (
     <View style={styles.container}>
-      <Image
-        source={icon}
-        style={{
-          width: 200,
-          height: 100,
-          resizeMode: "contain",
-          // resizeMode:'repeat',
-          // resizeMode:'stretch',
-        }}
-      />
-      <Image
-        source={{
-          uri: "https://www.metacritic.com/a/img/catalog/provider/6/3/6-1-4763-13.jpg",
-        }}
-        style={{
-          width: 215,
-          height: 294,
-          resizeMode: "contain",
-          //las imagenes que son remotas no se muestran hasta que le pongas una resolucion de ancho y alto //!ESTO ES OBLIGATORIO!
-        }}
-      />
-      <Text style={{ color: "white" }}>Un cambio!</Text>
-
-      <Button //!ESTE BOTON ES EL NATIVO, NO SE PUEDE ESTILAR
-        title="Click me!"
-        onPress={() => alert("Hola!")}
-      />
-      <TouchableHighlight //!ESTE BOTON SI SE PUEDE ESTILAR
-        underlayColor={"#fff"}
-        style={{
-          backgroundColor: "red",
-          padding: 20,
-          borderRadius: 10,
-        }}
-        onPress={() => alert("Hola!")}
-      >
-        <Text>Pulsa aqui!</Text>
-      </TouchableHighlight>
-
-      <Pressable
-        onPress={() => {}}
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed ? "red" : "blue",
-          },
-          styles.wrappedCustom,
-        ]}
-      >
-        {({ pressed }) => (
-          <Text
-            style={{
-              fontSize: pressed ? 32 : 16,
-              color: pressed ? "gray" : "white",
-            }}
-          >
-            {pressed ? "Presionando" : "Presiona aqui!"}
-          </Text>
-        )}
-      </Pressable>
       <StatusBar style="light" />
+
+      {pokemon.map((pokemon) => (
+        <View key={pokemon.slug} style={styles.card}>
+          <Image source={{ uri: pokemon.image }} style={styles.image} />
+        </View>
+      ))}
     </View>
   );
 }
@@ -84,4 +31,71 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  card: {},
+  image: {
+    width: 107,
+    height: 147,
+    borderRadius: 10,
+  },
 });
+{
+  /* <Image
+source={icon}
+style={{
+  width: 200,
+  height: 100,
+  resizeMode: "contain",
+  // resizeMode:'repeat',
+  // resizeMode:'stretch',
+}}
+/>
+<Image
+source={{
+  uri: "https://www.metacritic.com/a/img/catalog/provider/6/3/6-1-4763-13.jpg",
+}}
+style={{
+  width: 215,
+  height: 294,
+  resizeMode: "contain",
+  //las imagenes que son remotas no se muestran hasta que le pongas una resolucion de ancho y alto //!ESTO ES OBLIGATORIO!
+}}
+/>
+<Text style={{ color: "white" }}>Un cambio!</Text>
+
+<Button //!ESTE BOTON ES EL NATIVO, NO SE PUEDE ESTILAR
+title="Click me!"
+onPress={() => alert("Hola!")}
+/>
+<TouchableHighlight //!ESTE BOTON SI SE PUEDE ESTILAR
+underlayColor={"#fff"}
+style={{
+  backgroundColor: "red",
+  padding: 20,
+  borderRadius: 10,
+}}
+onPress={() => alert("Hola!")}
+>
+<Text>Pulsa aqui!</Text>
+</TouchableHighlight>
+
+<Pressable
+onPress={() => {}}
+style={({ pressed }) => [
+  {
+    backgroundColor: pressed ? "red" : "blue",
+  },
+  styles.wrappedCustom,
+]}
+>
+{({ pressed }) => (
+  <Text
+    style={{
+      fontSize: pressed ? 32 : 16,
+      color: pressed ? "gray" : "white",
+    }}
+  >
+    {pressed ? "Presionando" : "Presiona aqui!"}
+  </Text>
+)}
+</Pressable> */
+}
--- reactNative0/lib/metacritic.js
+++ reactNative0/lib/metacritic.js
@@ -1,62 +1,102 @@
+// export async function getLatestGames() {
+//   const LATEST_GAMES =
+//     "https://internal-prod.apigee.fandom.net/v1/xapi/finder/metacritic/web?sortBy=-metaScore&productType=games&page=1&releaseYearMin=1958&releaseYearMax=2024&offset=0&limit=24&apiKey=1MOZgmNFxvmljaQR1X9KAij9Mo4xAY3u";
+
+//   const rawData = await fetch(LATEST_GAMES);
+//   const json = await rawData.json();
+
+//   const {
+//     data: { items },
+//   } = json;
+
+//   return items.map((item) => {
+//     const { description, slug, releaseDate, image, criticScoreSummary, title } =
+//       item;
+//     const { score } = criticScoreSummary;
+
+//     // crea la imagen
+//     const { bucketType, bucketPath } = image;
+//     const img = `https://www.metacritic.com/a/img/${bucketType}${bucketPath}`;
+
+//     return {
+//       description,
+//       releaseDate,
+//       score,
+//       slug,
+//       title,
+//       image: img,
+//     };
+//   });
+// }
+// export async function getGameDetails(slug) {
+//   const GAME_DETAILS = `https://internal-prod.apigee.fandom.net/v1/xapi/composer/metacritic/pages/games/${slug}/web?&apiKey=1MOZgmNFxvmljaQR1X9KAij9Mo4xAY3u`;
+
+//   const rawData = await fetch(GAME_DETAILS);
+//   const json = await rawData.json();
+
+//   const { components } = json;
+//   const { title, description, criticScoreSummary, images } = components[0];
+//   const { score } = criticScoreSummary;
+
+//   // get the card image
+//   const cardImage = images.find((image) => image.typeName === "cardImage");
+//   const { bucketType, bucketPath } = cardImage;
+//   const img = `https://www.metacritic.com/a/img/${bucketType}${bucketPath}`;
+
+//   const rawReviews = components[3].data.items;
+
+//   // get the reviews
+//   const reviews = rawReviews.map((review) => {
+//     const { quote, score, date, publicationName, author } = review;
+//     return { quote, score, date, publicationName, author };
+//   });
+
+//   return {
+//     img,
+//     title,
+//     slug,
+//     description,
+//     score,
+//     reviews,
+//   };
+// }
+
 export async function getLatestGames() {
-  const LATEST_GAMES =
-    "https://internal-prod.apigee.fandom.net/v1/xapi/finder/metacritic/web?sortBy=-metaScore&productType=games&page=1&releaseYearMin=1958&releaseYearMax=2024&offset=0&limit=24&apiKey=1MOZgmNFxvmljaQR1X9KAij9Mo4xAY3u";
+  // Obtener los primeros 24 Pokémon (para mantener la misma cantidad que la función original)
+  const pokemonList = [];
 
-  const rawData = await fetch(LATEST_GAMES);
-  const json = await rawData.json();
+  for (let i = 1; i <= 24; i++) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    const pokemon = await response.json();
 
-  const {
-    data: { items },
-  } = json;
+    pokemonList.push({
+      title: pokemon.name,
+      description: `Order: ${pokemon.order}`,
+      releaseDate: null, // La PokeAPI no tiene fecha de lanzamiento
+      score: pokemon.base_experience, // Usamos base_experience como "score"
+      slug: pokemon.name,
+      image: pokemon.sprites.front_default,
+      types: pokemon.types.map((type) => type.type.name),
+    });
+  }
 
-  return items.map((item) => {
-    const { description, slug, releaseDate, image, criticScoreSummary, title } =
-      item;
-    const { score } = criticScoreSummary;
-
-    // crea la imagen
-    const { bucketType, bucketPath } = image;
-    const img = `https://www.metacritic.com/a/img/${bucketType}${bucketPath}`;
-
-    return {
-      description,
-      releaseDate,
-      score,
-      slug,
-      title,
-      image: img,
-    };
-  });
+  return pokemonList;
 }
-export async function getGameDetails(slug) {
-  const GAME_DETAILS = `https://internal-prod.apigee.fandom.net/v1/xapi/composer/metacritic/pages/games/${slug}/web?&apiKey=1MOZgmNFxvmljaQR1X9KAij9Mo4xAY3u`;
 
-  const rawData = await fetch(GAME_DETAILS);
-  const json = await rawData.json();
+export async function getGameDetails(pokemonName) {
+  const POKEMON_DETAILS = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
 
-  const { components } = json;
-  const { title, description, criticScoreSummary, images } = components[0];
-  const { score } = criticScoreSummary;
-
-  // get the card image
-  const cardImage = images.find((image) => image.typeName === "cardImage");
-  const { bucketType, bucketPath } = cardImage;
-  const img = `https://www.metacritic.com/a/img/${bucketType}${bucketPath}`;
-
-  const rawReviews = components[3].data.items;
-
-  // get the reviews
-  const reviews = rawReviews.map((review) => {
-    const { quote, score, date, publicationName, author } = review;
-    return { quote, score, date, publicationName, author };
-  });
+  const response = await fetch(POKEMON_DETAILS);
+  const pokemon = await response.json();
 
   return {
-    img,
-    title,
-    slug,
-    description,
-    score,
-    reviews,
+    img: pokemon.sprites.front_default,
+    title: pokemon.name,
+    slug: pokemon.name,
+    description: `Order: ${pokemon.order}`,
+    score: pokemon.base_experience,
+    types: pokemon.types.map((type) => type.type.name),
+    // Como no hay reviews en la PokeAPI, podemos retornar un array vacío o omitir esta propiedad
+    reviews: [],
   };
 }
